1f2d4cd711daccb81f93e5fdf89b66e771c1510a
allanndunda <allanndunda>
2025-09-17 21:02:31 +0300

    aaded

--- index.js
+++ index.js
@@ -12,5 +12,21 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(" ")
+      .map(word => {
+       
+        if (word === word.toUpperCase()) {
+          return word;
+        }
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  });
+};
+
+
+// const firstWord = titleCased.map(tutorials) => {
+//   return tutorials
+// }
